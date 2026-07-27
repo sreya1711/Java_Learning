0d51de8976084c926ab40d5fd2e422ed2d60e3d5
sreya <aishu@gmail.com>
2026-07-27 20:06:03 +0530

    'Average'

---
+++ Operators/Average.java
@@ -0,0 +1,10 @@
+public class Average{
+    public static void main(String[] args){
+        int a=10;
+        int b=15;
+        int c=20;
+        int sum=a+b+c;
+        int avg=sum/3;
+        System.out.println(avg);
+    }
+}
