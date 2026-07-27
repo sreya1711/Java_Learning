1dd2990f232dc01b2994b317a0d8919696667f52
sreya <aishu@gmail.com>
2026-07-27 14:38:31 +0530

    'Intro'

---
+++ Variables_Datatypes/Datatype.java
@@ -0,0 +1,12 @@
+public class Datatype{
+    public static void main(String[] args){
+        int a=10;
+        double b=2.5;
+        char c='A';
+        boolean d=true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
---
+++ Variables_Datatypes/Variable.java
@@ -0,0 +1,6 @@
+public class Variable{
+    public static void main(String[] args){
+        int age=21;
+        System.out.println(age);
+    }
+}
