4fecff0f382a50f35ce00d8f52c4de5ecccc851a
sreya <aishu@gmail.com>
2026-07-28 14:56:40 +0530

    'Unary'

---
+++ Operators/Unaryoperator.java
@@ -0,0 +1,10 @@
+public class Unaryoperator{
+    public static void main(String[] args) {
+        int a = 7;
+
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(--a);
+    }
+}
