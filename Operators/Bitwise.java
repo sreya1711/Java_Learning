b334c021b86a46c9623fd5bfddafd474b5751717
sreya <aishu@gmail.com>
2026-07-30 21:22:46 +0530

    'Bitwise'

---
+++ Operators/Bitwise.java
@@ -0,0 +1,16 @@
+public class Bitwise {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 3;
+
+        System.out.println("a & b  = " + (a & b));
+        System.out.println("a | b  = " + (a | b));
+        System.out.println("a ^ b  = " + (a ^ b));
+        System.out.println("~a     = " + (~a));
+        System.out.println("a << 1 = " + (a << 1));
+        System.out.println("a >> 1 = " + (a >> 1));
+
+    }
+}
