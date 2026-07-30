a9f0f33c7b1e81c2c69592657a78e4c0b100415c
sreya <aishu@gmail.com>
2026-07-30 21:08:57 +0530

    'ternary'

---
+++ Operators/Ternary.java
@@ -0,0 +1,7 @@
+public class Ternary{
+    public static void main(String[] args) {
+        int marks = 75;
+        String result = (marks >= 40) ? "Pass" : "Fail";
+        System.out.println(result);
+    }
+}
