d51009cc3f3e5b0fa112e75db46b32eea55779dd
sreya <aishu@gmail.com>
2026-08-04 11:33:51 +0530

    'ifelse'

---
+++ Conditional_Statements/Ifelse.java
@@ -0,0 +1,15 @@
+public class Ifelse {
+
+    public static void main(String[] args) {
+
+        int age = 16;
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+
+        System.out.println("Program Ended.");
+    }
+}
