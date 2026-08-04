862e29d4b0d4afb64fde40f9349c6c36dc19b8c6
sreya <sreyarajesh110805@gmail.com>
2026-08-04 15:01:07 +0530

    rectangle pattern

---
+++ Loops/Nestedloop.java
@@ -0,0 +1,18 @@
+public class Nestedloop {
+
+    public static void main(String[] args) {
+
+        for (int row = 1; row <= 4; row++) {
+
+            for (int col = 1; col <= 5; col++) {
+
+                System.out.print("* ");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+}
