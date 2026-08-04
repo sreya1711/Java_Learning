53b11f05604697c9bc821c6aabd84e113d2d6716
sreya <sreyarajesh110805@gmail.com>
2026-08-04 20:50:33 +0530

    break statement

---
+++ Loops/Break.java
@@ -0,0 +1,16 @@
+public class Break {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+
+            if (i == 6) {
+                break;
+            }
+
+            System.out.println(i);
+        }
+
+        System.out.println("Loop Ended");
+    }
+}
