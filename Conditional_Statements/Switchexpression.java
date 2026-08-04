cb795d0e39eb5b6c0fdb57e27a72e39f192f670f
sreya <aishu@gmail.com>
2026-08-04 12:17:04 +0530

    'switchexpression'

---
+++ Conditional_Statements/Switchexpression.java
@@ -0,0 +1,19 @@
+public class Switchexpression {
+
+    public static void main(String[] args) {
+
+        int day = 2;
+
+        switch (day) {
+
+            case 1 -> System.out.println("Monday");
+
+            case 2 -> System.out.println("Tuesday");
+
+            case 3 -> System.out.println("Wednesday");
+
+            default -> System.out.println("Invalid Day");
+        }
+
+    }
+}
