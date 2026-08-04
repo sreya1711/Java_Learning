ebc0cddb2c1cb6291d687f8d0b64da5621b0dc71
sreya <aishu@gmail.com>
2026-08-04 11:15:14 +0530

    'conditional'

---
+++ Conditional_Statements/Ifstatement.java
@@ -0,0 +1,13 @@
+public class Ifstatement {
+
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        }
+
+        System.out.println("Program Ended.");
+    }
+}
