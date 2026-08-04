6cf37f271eda58f2b4929c1d71a43e0571792558
sreya <aishu@gmail.com>
2026-08-04 11:49:34 +0530

    'elseifladder'

---
+++ Conditional_Statements/Elseifladder.java
@@ -0,0 +1,19 @@
+public class Elseifladder {
+
+    public static void main(String[] args) {
+
+        int marks = 85;
+
+        if (marks >= 90) {
+            System.out.println("Grade A");
+        } else if (marks >= 80) {
+            System.out.println("Grade B");
+        } else if (marks >= 70) {
+            System.out.println("Grade C");
+        } else if (marks >= 40) {
+            System.out.println("Grade D");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
